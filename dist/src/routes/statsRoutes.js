c4481956f176427783db5d3580d6ffd35f7b5d2d
ijazkhan0313 <96014060+ijazkhan0313@users.noreply.github.com>
2024-10-03 00:10:12 -0700

    afdsasdf

--- dist/src/routes/statsRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const stats_controller_1 = require("../controllers/stats.controller");
-async function statsRoutes(router) {
-    router.get('/p2base/admin/stats', stats_controller_1.getStats);
-    router.get('/p2base/webVisitLog', stats_controller_1.webSiteVisite);
-}
-module.exports = statsRoutes;
