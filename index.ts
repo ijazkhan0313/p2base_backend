83d7189402a8a22e86d3951b7a99d4ac3b6f49d5
ijazkhan0313 <96014060+ijazkhan0313@users.noreply.github.com>
2024-10-04 05:02:51 -0700

    asdfasdf

--- index.ts
+++ index.ts
@@ -44,6 +44,23 @@ server.register.use((req:any, res:any, next:any) => {
 });
 
 
+const  fn  = async (req:any, res:any) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+  return await fn(req, res)
+}
+
 //TODO Add getting satic img to controllers
 server.get('/events/:filename', (req: any,rep: any) => {
   const filename = req.params.filename
